f27f17e09e0449380cf0c1c6e1373a5059079bdf
Densatho <dsjacinthotre@gmail.com>
2021-04-01 18:53:55 +0000

    Implementação do webhook

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,14 @@
 module.exports = {
     name: 'ping',
     description: 'This is a ping command.',
-    execute(message, args){
-        message.reply('pong!');
+    execute(message, embed, webHook, args){        
+        embed.setTitle('Pong')
+        embed.setColor('#008000')
+        webHook.send('', {
+            username: 'ChoronoOne',
+            avatarURL: 'https://imgur.com/M2uFwtY.png',
+            embeds: [embed],
+        })
     }
     
 }
--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-
-const prefix = '-c ' 
-
+const embed = new Discord.MessageEmbed()
+const prefix = '-c '
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command)
@@ -19,38 +18,26 @@ client.once('ready', () => {
     console.log('ChronoOne is ready!');
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift();
 
+    if (client.commands.has(command)) {
+        message.channel.createWebhook('ChoronoOne', reason = "Send message").then(webHook => {
+            console.log(`> command accepted is ${command}`)
+            client.commands.get(command).execute(message, embed, webHook, args);
+        })
+    }
+
     message.channel.fetchWebhooks().then(hooks => {
-
-        if (hooks.size === 0){
-            message.channel.createWebhook('ChoronoOne', reason = "need a cool new Webhook")
-            console.log(`> create a new WebHook`)
-        } else {
-            hooks.forEach(valor => {
-                console.log(`> valor = ${valor.name}`)
-                if (valor.name === 'ChoronoOne'){
-                    console.log(`> ChoronoOne is already created`)
-                } else {
-                    message.channel.createWebhook('ChoronoOne', reason = "need a cool new Webhook")
-                    console.log(`> create a new WebHook`)
-                }
-            })
-        }
-
+        hooks.forEach(element => {
+            if (element.name === 'ChoronoOne')
+                element.delete('End execution.')
+        });
     })
 
-    if(client.commands.has(command)){
-        console.log(`> command accepted is ${command}`)
-        client.commands.get(command).execute(message, args);
-        }
-    });
-
+})
 
 client.login('ODIyMjE0ODc4MDcyNjY4MTgw.YFPBRg.ECaLQuM61bWEeX-eb0AAz8y5CxE');
-
-
