15e3098089636ed2941d58d843bd6e83b1108ce5
Densatho <Densatho@github.com>
2021-08-15 16:32:01 -0300

    Ajustando database_setup.js

--- database_ops/database_setup.js
+++ database_ops/database_setup.js
@@ -2,14 +2,13 @@ import database from "../lib/database";
 import folder from "./folderDB";
 import { createPrefix } from "./prefix";
 
-databaseSetup(serverId)
-{
-    database.database().ref(folder+'/'+serverId).set({
-        prefix: createPrefix(serverId)
-    })  
+function databaseSetup(serverId) {
+  database
+    .database()
+    .ref(folder + "/" + serverId)
+    .set({
+      prefix: createPrefix(serverId),
+    });
 }
 
 export default databaseSetup;
-
-
-
