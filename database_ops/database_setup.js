3ae295a20897858091c0b5e89b4a0f4da504e677
Lucas Azzi <lucashazzi@gmail.com>
2021-08-15 16:42:08 -0300

    module exports no db setup

--- database_ops/database_setup.js
+++ database_ops/database_setup.js
@@ -2,13 +2,13 @@ import database from "../lib/database";
 import folder from "./folderDB";
 import { createPrefix } from "./prefix";
 
-function databaseSetup(serverId) {
-  database
-    .database()
-    .ref(folder + "/" + serverId)
-    .set({
-      prefix: createPrefix(serverId),
-    });
-}
-
-export default databaseSetup;
+module.exports = {
+  databaseSetup(serverId) {
+    database
+      .database()
+      .ref(folder + "/" + serverId)
+      .set({
+        prefix: createPrefix(serverId),
+      });
+  },
+};
